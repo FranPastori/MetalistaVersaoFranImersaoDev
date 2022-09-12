a0583866e8a9dd154c7d1fe341dadc228c98a276
FranPastori <franciele_pastori@hotmail.com>
2022-09-12 13:23:00 -0300

    resolvi bug clicadas

--- app.js
+++ app.js
@@ -6,37 +6,37 @@ var clicou3= 0
 var clicou4= 0
 var clicou5= 0
 function tabela1Click(){
-var nClicadas;
-nClicadas = clicou +1;
-if (nClicadas <=1){
+var nClicadas=1
+nClicadas = clicou++ ;
+if (nClicadas ==0){
     numeroPensado = numeroPensado + valortabelas[0];
 }
 }
 function tabela2Click(){
-    var nClicadas2;
-    nClicadas2 = clicou2 +1;
-    if (nClicadas2 <=1){
+    var nClicadas2=1
+    nClicadas2 = clicou2++;
+    if (nClicadas2 ==0){
         numeroPensado = numeroPensado + valortabelas[1];
     }
 }
 function tabela3Click(){
     var nClicadas3;
-    nClicadas3 = clicou3 +1;
-    if (nClicadas3 <=1){
+    nClicadas3 = clicou3++;
+    if (nClicadas3==0){
         numeroPensado = numeroPensado + valortabelas[2];
     }
 }
 function tabela4Click(){
     var nClicadas4;
-    nClicadas4= clicou4 +1;
-    if (nClicadas4 <=1){
+    nClicadas4= clicou4++;
+    if (nClicadas4 ==0){
         numeroPensado = numeroPensado + valortabelas[3];
     }
 }
 function tabela5Click(){
     var nClicadas5;
-    nClicadas5 = clicou5 +1;
-    if (nClicadas5 <=1){
+    nClicadas5 = clicou5++;
+    if (nClicadas5 ==0){
         numeroPensado = numeroPensado + valortabelas[4];
     }
  }
