ee5b116adca02cee55bd1dedfc603c029ac53441
FranPastori <franciele_pastori@hotmail.com>
2022-09-12 11:43:47 -0300

    limitando numeros de click tentativa 2

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-
 var valortabelas = [16,1,4,8,2]
 var numeroPensado = 0
+var clicou = 0
 function tabela1Click(){
-numeroPensado = numeroPensado + valortabelas[0]
+var nClicadas;
+nClicadas= clicou+1;
+if (nClicadas <=1){
+    numeroPensado = numeroPensado + valortabelas[0];
+}
 }
 function tabela2Click(){
-    numeroPensado = numeroPensado + valortabelas[1]
+var clicou2 = 1
+if (clicou2<=1){
+numeroPensado = numeroPensado + valortabelas[1]
+    }
 }
 function tabela3Click(){
-    numeroPensado = numeroPensado + valortabelas[2]
+    var clicou3 = 1
+    if (clicou3<=1){
+        numeroPensado = numeroPensado + valortabelas[2]
+    }
 }
 function tabela4Click(){
+    var clicou4 = 1
+if (clicou4<=1){
     numeroPensado = numeroPensado + valortabelas[3]
 }
+}
 function tabela5Click(){
-numeroPensado = numeroPensado + valortabelas[4]
+    var clicou5= 1
+    if (clicou5<=1){
+        numeroPensado = numeroPensado + valortabelas[4]
+    }
  }
 function mostra(){
     console.log(numeroPensado)
